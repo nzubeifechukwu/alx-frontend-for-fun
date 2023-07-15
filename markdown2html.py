1bb34d92f4628559bb038cffb5c65e17c9d6cbf1
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-15 05:24:32 +0100

    Add markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -16,4 +16,23 @@ if __name__ == '__main__':
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
 
+    sym_map = {
+            '# ': ('<h1>', '</h1>\n'),
+            '## ': ('<h2>', '</h2>\n'),
+            '### ': ('<h3>', '</h3>\n'),
+            '#### ': ('<h4>', '</h4>\n'),
+            '##### ': ('<h5>', '</h5>\n'),
+            '###### ': ('<h6>', '</h6>\n')
+        }
+
+    with open(sys.argv[1]) as rf:
+        read_lines = rf.readlines()
+        with open(sys.argv[2], 'a') as wf:
+            for line in read_lines:
+                for k, v in sym_map.items():
+                    if line.startswith(k):
+                        html_line = line.replace(k, v[0]).replace('\n', v[1])
+                        break
+                wf.write(html_line)
+
     sys.exit(0)
